55259c12898d00de3a70d059842c8c27928e8962
rummik <k@9k1.us>
2017-12-31 06:10:51 -0500

    Motor test

--- app.js
+++ app.js
@@ -1,22 +1,69 @@
 const GPIO = require('onoff').Gpio;
+const pify = require('pify');
 
-const senseWhite = new GPIO(20, 'in', 'both');
-const senseBlue = new GPIO(21, 'in', 'both');
-const senseHome = new GPIO(16, 'in', 'both');
+const timeout = pify(setTimeout);
 
-var whiteLast = 0;
-var blueLast = 0;
+const home = new GPIO(16, 'in', 'both');
+
+const decoder = {
+	channelA: new GPIO(19, 'in', 'both'),
+	channelB: new GPIO(21, 'in', 'both'),
+};
+
+const motor = new class Motor {
+	constructor() {
+		this.hbridge = {
+			switch1: pify(new GPIO(6, 'out')),
+			switch2: pify(new GPIO(13, 'out')),
+			switch3: pify(new GPIO(19, 'out')),
+			switch4: pify(new GPIO(26, 'out')),
+		};
+
+		this.reset();
+	}
+
+	reset() {
+		return Promise.all([
+			this.hbridge.switch1.write(0),
+			this.hbridge.switch2.write(0),
+			this.hbridge.switch3.write(0),
+			this.hbridge.switch4.write(0),
+		]);
+	}
+
+	forward() {
+		return this.reset().then(() => Promise.all([
+			this.hbridge.switch1.write(1),
+			this.hbridge.switch4.write(1),
+		]));
+	}
+
+	reverse() {
+		return this.reset().then(() => Promise.all([
+			this.hbridge.switch2.write(1),
+			this.hbridge.switch3.write(1),
+		]));
+	}
+
+	brake() {
+		return this.reset().then(() => Promise.all([
+			this.hbridge.switch1.write(1),
+			this.hbridge.switch3.write(1),
+		]));
+	}
+};
 
 var count = 0;
 
-senseWhite.watch((err, value) => {
+decoder.channelA.watch((err, value) => {
 	count++;
 });
 
-senseBlue.watch((err, value) => {
-});
-
-senseHome.watch((err, value) => {
+home.watch((err, value) => {
 	console.log('COUNT:', count);
 	count = 0;
 });
+
+motor.forward()
+	.then(() => timeout(1000))
+	.then(() => motor.break());
