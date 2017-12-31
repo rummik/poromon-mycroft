bf7fb1b2dc737325660900211603baaf31569b18
rummik <k@9k1.us>
2017-12-31 08:23:14 -0500

    More testing

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const GPIO = require('onoff').Gpio;
 const pify = require('pify');
 
-const timeout = pify(setTimeout);
+const conf = { include: [ 'write' ] };
+
+const timeout = pify((timeout, cb) => setTimeout(cb, timeout));
 
 const home = new GPIO(16, 'in', 'both');
 
@@ -13,10 +15,8 @@ const decoder = {
 const motor = new class Motor {
 	constructor() {
 		this.hbridge = {
-			switch1: pify(new GPIO(6, 'out')),
-			switch2: pify(new GPIO(13, 'out')),
-			switch3: pify(new GPIO(19, 'out')),
-			switch4: pify(new GPIO(26, 'out')),
+			inA: pify(new GPIO(19, 'out'), conf),
+			inB: pify(new GPIO(26, 'out'), conf),
 		};
 
 		this.reset();
@@ -24,32 +24,28 @@ const motor = new class Motor {
 
 	reset() {
 		return Promise.all([
-			this.hbridge.switch1.write(0),
-			this.hbridge.switch2.write(0),
-			this.hbridge.switch3.write(0),
-			this.hbridge.switch4.write(0),
+			this.hbridge.inA.write(0),
+			this.hbridge.inB.write(0),
 		]);
 	}
 
 	forward() {
-		return this.reset().then(() => Promise.all([
-			this.hbridge.switch1.write(1),
-			this.hbridge.switch4.write(1),
-		]));
+		return this.reset().then(() => {
+			return this.hbridge.inA.write(1);
+		});
 	}
 
 	reverse() {
-		return this.reset().then(() => Promise.all([
-			this.hbridge.switch2.write(1),
-			this.hbridge.switch3.write(1),
-		]));
+		return this.reset().then(() => {
+			return this.hbridge.inB.write(1);
+		});
 	}
 
 	brake() {
-		return this.reset().then(() => Promise.all([
-			this.hbridge.switch1.write(1),
-			this.hbridge.switch3.write(1),
-		]));
+		return Promise.all([
+			this.hbridge.inA.write(1),
+			this.hbridge.inB.write(1),
+		]);
 	}
 };
 
@@ -62,8 +58,10 @@ decoder.channelA.watch((err, value) => {
 home.watch((err, value) => {
 	console.log('COUNT:', count);
 	count = 0;
+
+	motor.brake()
+		.then(() => timeout(100))
+		.then(() => motor.reset());
 });
 
-motor.forward()
-	.then(() => timeout(1000))
-	.then(() => motor.break());
+motor.forward();
