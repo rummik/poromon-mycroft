a1b741029467b9ad1a0d2fa0d05ec9716bd1e13e
rummik <k@9k1.us>
2017-12-31 03:18:05 -0500

    Counting

--- app.js
+++ app.js
@@ -4,14 +4,19 @@ const senseWhite = new GPIO(20, 'in', 'both');
 const senseBlue = new GPIO(21, 'in', 'both');
 const senseHome = new GPIO(16, 'in', 'both');
 
+var whiteLast = 0;
+var blueLast = 0;
+
+var count = 0;
+
 senseWhite.watch((err, value) => {
-	console.log('WHITE:', value);
+	count++;
 });
 
 senseBlue.watch((err, value) => {
-	console.log('BLUE:', value);
 });
 
 senseHome.watch((err, value) => {
-	console.log('HOME:', value);
+	console.log('COUNT:', count);
+	count = 0;
 });
